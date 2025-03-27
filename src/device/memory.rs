07142ead99992062702033525fe38ed4d8a8a946
Bryn Mailer <bryndiupero@gmail.com>
2025-03-28 00:49:49 +1000

    feat: implemented Memory::write

--- src/device/memory.rs
+++ src/device/memory.rs
@@ -1,4 +1,21 @@
+use std::{error::Error, fmt};
+
 const MEMORY_SIZE: usize = 0x1000; // 4kB
+
+#[derive(Debug)]
+enum MemoryError {
+    OutOfBounds(u16),
+}
+
+impl fmt::Display for MemoryError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            MemoryError::OutOfBounds(addr) => write!(f, "attempt to write to memory failed: address {} out of bounds", addr),
+        }
+    }
+}
+
+impl Error for MemoryError {}
 
 pub struct Memory {
     ram: [u8; MEMORY_SIZE],
@@ -11,7 +28,16 @@ impl Memory {
         }
     }
 
-    pub fn write_to_addr(&self, data: &[u8], addr: u16) -> Result<(), MemoryError> {
-        todo!();
+    pub fn write(
+        &mut self,
+        data: u8,
+        addr: u16,
+    ) -> Result<(), MemoryError> {
+        if addr as usize > MEMORY_SIZE - 1 {
+            Err(MemoryError::OutOfBounds(addr))
+        } else {
+            self.ram[addr as usize] = data;
+            Ok(())
+        }
     }
 }
